8aff25651e32de10a3b117800b2d794494b31440
shashwatcs <yshashwat0@gmail.com>
2026-07-20 02:29:27 +0530

    Time: 7 ms (30.26%), Space: 24.7 MB (14.64%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -16,6 +16,5 @@ class Solution(object):
         # Recursively find the depth of left and right subtrees
         left_depth = self.maxDepth(root.left)
         right_depth = self.maxDepth(root.right)
-
-        # Total depth is the max of the two paths plus 1 for the current node
+        
         return max(left_depth, right_depth) + 1
