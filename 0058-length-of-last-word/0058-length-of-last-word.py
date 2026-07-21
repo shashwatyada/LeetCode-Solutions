6d876d6679fb8aef2d0adf29cf90d3d3ddb516a9
shashwatcs <yshashwat0@gmail.com>
2026-07-21 16:58:00 +0530

    Time: 0 ms (100%), Space: 12.3 MB (78.68%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        length = 0
+        i = len(s) -1 
+
+        while i >=0 and s[i] == ' ':
+            i -=1
+
+        while i >= 0 and s[i] != ' ':
+            length +=1
+            i-=1
+
+        return length
