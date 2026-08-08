447f076cf9f7ffa2c616e6df481e04818e652b8e
shashwatcs <yshashwat0@gmail.com>
2026-08-08 08:54:53 +0530

    Time: 3 ms (67.12%), Space: 16.8 MB (96.76%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -10,16 +10,22 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: bool
         """
-        def validate(node, low = float('-inf'), high = float('inf')):
-            # An empty tree/node is a valid BST 
-            if not node:
-                return True
-
-            # current node value must strictly be within (low, high)
-            if not(low < node.val < high):
+        stack = []
+        curr = root
+        prev_val = float('-inf')
+        
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+                
+            curr = stack.pop()
+            
+            # Value must be strictly greater than the previously visited node
+            if curr.val <= prev_val:
                 return False
-
-            # Left child must be < node.val, right child must be > node.val
-            return validate(node.left, low, node.val) and validate( node.right, node.val, high)
-
-        return validate(root)
+            prev_val = curr.val
+            
+            curr = curr.right
+            
+        return True
